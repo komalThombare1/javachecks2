0956315c71390fee2f6f210dcedce05584f0658f
komalThombare1 <100417869+komalThombare1@users.noreply.github.com>
2022-04-05 17:42:04 +0530

    Create CrashedFinalizable.java

---
+++ CrashedFinalizable.java
@@ -0,0 +1,13 @@
+public class CrashedFinalizable { 
+@Override
+public void finalize() { // +1 VIOLATION 
+System.out.print("");
+}
+}
+
+public class CrashedFinalizable_2 { 
+@Override
+def finalize() { // +1 VIOLATION (public is the default visibility)
+System.out.print("");
+}
+}
